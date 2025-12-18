f831a39f58eee6fba768a6850f0dc291b80a0dc0
TanishSharma151 <tanishrajsharma151@gmail.com>
2025-12-19 01:10:48 +0530

    Add JSON middleware and test route

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -2,8 +2,17 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/health", (req, res) => {
   res.send("API is running");
+});
+
+app.post("/test", (req, res) => {
+  res.json({
+    message: "POST route working",
+    body : req.body,
+  })
 });
 
 app.listen(3000, () => {
