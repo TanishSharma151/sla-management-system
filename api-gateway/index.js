018081538a9bf66e228e5e66e9cfec2adda94742
TanishSharma151 <tanishrajsharma151@gmail.com>
2025-12-20 00:15:19 +0530

    Fix auth route wiring

--- api-gateway/index.js
+++ api-gateway/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
+const authRoutes = require("./routes/auth.routes");
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/health", (req, res) => {
@@ -14,6 +14,8 @@ app.post("/test", (req, res) => {
     body : req.body,
   })
 });
+
+app.use("/auth", authRoutes);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
